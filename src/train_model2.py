87b024f1cce18bbd68bd1a070a5f95b18bb4c8a9
Luís Oliveira <luis.miguel5862004@gmail.com>
2025-06-13 15:31:18 +0100

    update model

--- src/train_model2.py
+++ src/train_model2.py
@@ -4,6 +4,7 @@ import glob
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 
@@ -226,7 +227,7 @@ def main_menu():
     X_scaled = scaler.fit_transform(X)
     X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
-    model = LogisticRegression()
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
 
     print("✅ Modelo treinado com acuccary:", accuracy_score(y_test, model.predict(X_test)))
